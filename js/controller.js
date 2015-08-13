8c8a47a9fc06f11c329e2ca5eae8f0c0421444b8
Ivan Petrovic <ipet@mail.com>
2015-08-13 13:26:46 +0200

    Adde comments to controller

--- js/controller.js
+++ js/controller.js
@@ -11,6 +11,9 @@ $(document).ready(function() {
 		tag: 'boards',
 		scope: {
 			boards: boards,
+			/*
+				crate empty board from input value and push it to boards List
+			*/
 			createBoard: function(context, el, value){
 				var value = can.trim(el.val());
 
@@ -28,6 +31,9 @@ $(document).ready(function() {
 				}
 
 			},
+			/*
+				render one board view
+			*/
 			openBoard: function (context, el, value) {
 				$('#content').html(can.view('board-template', {board: context}));
 
@@ -39,6 +45,10 @@ $(document).ready(function() {
 	can.Component.extend({
 		tag: 'board',
 		scope: {
+			/*
+				create new list with value from input element
+				and push it to lists List in current board
+			*/
 			createList: function(context, el, value){
 
 				var value = can.trim(el.val());
@@ -59,6 +69,11 @@ $(document).ready(function() {
 					el.val('');
 				}
 			},
+			/*
+				create new task with value from input element
+				with init values  and push it to tasks List in 
+				current list
+			*/
 			createTask: function(context, el, value){
 				var value = can.trim(el.val());
 
@@ -80,10 +95,16 @@ $(document).ready(function() {
 					el.val('');
 				}
 			},
+			/*
+				render view for one task with start-stop button
+			*/
 			openTask: function (context, parentContext) {
 				var boardId = parentContext.id;
 				$('#content').html(can.view('task-template', {task: context, parent: boardId}));
 			},
+			/*
+				returns to first page with all boards
+			*/
 			goBack: function(context, el, value){
 				$('#content').html(can.view('boards-template', {}));
 			}
@@ -95,6 +116,12 @@ $(document).ready(function() {
 		tag: 'task',
 		scope: {
 			boards: boards,
+			/*
+				startTimer will setInterval for model at count each second
+				that it is running. 
+				Attribute counter will contain setInterval
+				Attribute started will contain current State
+			*/
 			startTimer: function(context, el, value){
 				context.attr('started', true);
 				context.attr('counter', setInterval(function(task){
@@ -102,11 +129,19 @@ $(document).ready(function() {
 					task.attr('total', current + 1);
 				}, 1000, context));
 			},
+			/*
+				stopTimer will clearInterval for attribute counter
+				and set started to false
+			*/
 			stopTimer: function(context, el, value){
 				clearInterval(context.attr('counter'));
 				context.attr('started', false);
 				context.save();
 			},
+			/*
+				stopTimer will clearInterval for attribute counter
+				and set started to false
+			*/
 			goBack: function(context, el, value){
 				this.boards.each(function(board){
 					if(board.id == context){
